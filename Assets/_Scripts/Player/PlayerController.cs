b7b4bfb71ad771486a86c20cd721c234ed5eb8ce
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2022-10-26 14:57:40 -0300

    GR_configuracion-game-and-fix-sprint
    Se agrego la configuracion de gameplay del player, para poder editar la sensibilidad y los inverted axis.
    Ademas se reparo el sprint del player que lo hacia tambien cuando estaba en el aire

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -12,6 +12,7 @@ namespace Game.Player
         [SerializeField, Range(0, 20)] float _sprintSpeed = 12f;
         [SerializeField] PlayerInput _playerInput;
         [SerializeField] PlayerAnimationManager _animator;
+        [SerializeField] WeaponController _weaponController;
         Vector2 _moveVelocityInput;
         float _currentTime = 1;
         float _time;
@@ -28,6 +29,18 @@ namespace Game.Player
             set => _currentSpeed = value;
         }
 
+        public float NormalSpeed
+        {
+            get => _normalSpeed;
+        }
+        
+        public float SprintSpeed
+        {
+            get => _sprintSpeed;
+        }
+
+        public bool CanSprint { get; set; } = true;
+
         #region unitymethods
 
         void Awake()
@@ -43,7 +56,16 @@ namespace Game.Player
 
         void Update()
         {
-                _moveComponent.Velocity = (_moveVelocityInput.x * transform.right + transform.forward * _moveVelocityInput.y).normalized * _currentSpeed;
+            if (!_jumpComponent.IsOnTheFloor)
+            {
+                SprintActive(false);
+                CanSprint = false;
+            }
+            else
+            {
+                CanSprint = true;
+            }
+            _moveComponent.Velocity = (_moveVelocityInput.x * transform.right + transform.forward * _moveVelocityInput.y).normalized * _currentSpeed;
             /*if ((_currentTime < 0 || _jumpComponent.IsOnTheFloor))
             {
             }
@@ -86,12 +108,27 @@ namespace Game.Player
 
         public void SprintInput(InputAction.CallbackContext context)
         {
+            if (!CanSprint) return;
             if (context.started)
+            {
+                SprintActive(true);
+            }
+            else if (context.canceled)
+            {
+                SprintActive(false);
+                
+            }
+        }
+
+        void SprintActive(bool active)
+        {
+            _weaponController.CanAttack = !active;
+            if (active)
             {
                 _currentSpeed = _sprintSpeed;
                 _animator.StartSprint();
             }
-            else if (context.canceled)
+            else
             {
                 _currentSpeed = _normalSpeed;
                 _animator.StopSprint();
--- Assets/_Scripts/Player/WeaponController.cs
+++ Assets/_Scripts/Player/WeaponController.cs
@@ -11,25 +11,27 @@ namespace Game.Player
         [SerializeField] PointerTarget _pointerTarget;
         [SerializeField] PlayerController _playerController;
         [SerializeField, Range(0, 2)] float _speedWeaponHeavy = 1;
-        private float _speedDefault;
         private Weapon _currentWeapon;
+        public bool CanAttack { get; set; } = true;
 
         private void Awake()
         {
             _currentWeapon = manager.CurrentWeapon;
-            _speedDefault = _playerController.Speed;
         }
 
         public void AttackInput(InputAction.CallbackContext context)
         {
+            if (!CanAttack) return;
             _currentWeapon = manager.CurrentWeapon;
             _currentWeapon.Target = _pointerTarget.transform.position;
             if (context.started)
             {
                 if (_currentWeapon.IsHeavy)
                 {
-                    _playerController.Speed = _speedWeaponHeavy;
+                    PlayerHeavy();
                 }
+
+                _playerController.CanSprint = false;
                 _currentWeapon.StartAttack();
             }
             if (context.performed)
@@ -38,8 +40,9 @@ namespace Game.Player
             {
                 if (_currentWeapon.IsHeavy)
                 {
-                    _playerController.Speed = _speedDefault;
+                    PlayerBackToDefault();
                 }
+                _playerController.CanSprint = true;
                 _currentWeapon.CancelAttack();
             }
         }
@@ -68,11 +71,24 @@ namespace Game.Player
         public void SwitchWeapon(int slot)
         {
             _currentWeapon.CancelAttack();
-            _playerController.Speed = _speedDefault;
+            if (_currentWeapon.IsHeavy)
+                PlayerBackToDefault();
+            _playerController.CanSprint = true;
             manager.SwitchWeapon(slot);
             UpdateUI();
         }
-        private void UpdateUI()
+
+        void PlayerBackToDefault()
+        {
+            _playerController.Speed = _playerController.NormalSpeed;
+            _playerController.CanSprint = true;
+        }
+
+        void PlayerHeavy()
+        {
+            _playerController.Speed = _speedWeaponHeavy;
+        }
+        void UpdateUI()
         {
             var weapon = manager.CurrentWeapon;
             GameManager.instance.UpdateReserveCounter(weapon.ReserveAmmunition);
