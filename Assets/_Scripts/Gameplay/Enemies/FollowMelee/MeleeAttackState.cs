76f507ba74aa6d3641462236a71b1d6d2bc499bb
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2022-11-05 18:06:35 -0300

    Se corrigio el enemigo de madera para que tenga todo lo necesario.

--- Assets/_Scripts/Gameplay/Enemies/FollowMelee/MeleeAttackState.cs
+++ Assets/_Scripts/Gameplay/Enemies/FollowMelee/MeleeAttackState.cs
@@ -6,49 +6,49 @@ namespace Game.Gameplay.Enemies.FollowMelee
     public class MeleeAttackState : State
     {
         FollowMeleeStateController _stateController;
-        MeleeAttack _meleeAttack;
         NavMeshAgent _agent;
         LookAtTarget _lookAtTarget;
         AnimationEvent _animationEvent;
-        bool _isAttacking = false;
         GameObject _player;
         float _rangeMelee;
         float _rangeOfVisionY;
+        AnimatorController _animatorController;
+        bool _isAttacking = false;
 
         public MeleeAttackState(
             FollowMeleeStateController stateController,
-            MeleeAttack meleeAttack,
             NavMeshAgent agent,
             LookAtTarget lookAtTarget,
             AnimationEvent animationEvent,
             GameObject player,
             float rangeMelee,
-            float rangeOfVisionY
+            float rangeOfVisionY,
+            AnimatorController animatorController
         )
         {
             _stateController = stateController;
-            _meleeAttack = meleeAttack;
             _agent = agent;
             _lookAtTarget = lookAtTarget;
             _animationEvent = animationEvent;
             _player = player;
             _rangeMelee = rangeMelee;
             _rangeOfVisionY = rangeOfVisionY;
+            _animatorController = animatorController;
         }
 
         public override void Enter()
         {
             _agent.speed = 0;
-            _meleeAttack.enabled = true;
             _lookAtTarget.enabled = true;
             _animationEvent.OnAttackStarts += OnOnAttackStartsHandler;
             _animationEvent.OnAttackEnds += OnOnAttackEndsHandler;
-            _meleeAttack.Attack();
+            _animatorController.Attack();
         }
 
         public override void Update()
         {
             if (_isAttacking) return;
+
             Vector3 position = _stateController.transform.position;
             Vector3 playerPosition = _player.transform.position;
             if (!Utils.IsInRangeOfVision(position, playerPosition, _rangeMelee, _rangeOfVisionY))
@@ -57,12 +57,11 @@ namespace Game.Gameplay.Enemies.FollowMelee
             }
             else
             {
-                _meleeAttack.Attack();
+                _animatorController.Attack();
             }
         }
         public override void Exit()
         {
-            _meleeAttack.enabled = false;
             _lookAtTarget.enabled = false;
             _animationEvent.OnAttackStarts -= OnOnAttackStartsHandler;
             _animationEvent.OnAttackEnds -= OnOnAttackEndsHandler;
