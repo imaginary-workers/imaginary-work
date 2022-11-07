334792e733ad2604d58af9b913a865cf1352bf9a
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2022-11-06 23:16:39 -0300

    st

---
+++ Assets/_Scripts/SO/SceneStorage.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    [CreateAssetMenu(fileName = "SceneStorage", menuName = "Scriptable Object/config/scene storage", order = 0)]
+    public class SceneStorageSO : ScriptableObject
+    {
+        [SerializeField]List<SceneSO> _scenes;
+        public SceneSO FindSceneByName(string name)
+        {
+           return _scenes.Find((scene) => scene.SceneName == name);               
+        }
+
+    }
+}
