cad1ae97724b7612c849c2f2804fc24f69f1523d
Zebado <sbasualdo.antares@gmail.com>
2022-11-10 23:22:36 -0300

    sb

--- Assets/_Scripts/Gameplay/Player/WeaponsSoundController.cs
+++ Assets/_Scripts/Gameplay/Player/WeaponsSoundController.cs
@@ -8,10 +8,34 @@ namespace Game.Gameplay.Player
     {
         [SerializeField] AudioSource _audioSource;
         [SerializeField] AudioClip _switch;
+        [SerializeField] AudioClip _fire;
+        [SerializeField] AudioClip _noHit;
+        [SerializeField] AudioClip _shootPistol;
+        [SerializeField] AudioClip _recoveryPistol;
 
         public void SwitchWeapon()
         {
             _audioSource.PlayOneShot(_switch);
+        }
+
+        public void ShootFire()
+        {
+            _audioSource.PlayOneShot(_fire);
+        }
+
+        public void HitFail()
+        {
+            _audioSource.PlayOneShot(_noHit);
+        }
+
+        public void ShootPistol()
+        {
+            _audioSource.PlayOneShot(_shootPistol);
+        }
+
+        public void RecoveryPistol()
+        {
+            _audioSource.PlayOneShot(_recoveryPistol);
         }
     }
 }
