40420ba3208754ad9f870bb767f1eed1f34ddcce
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2022-11-18 15:03:28 -0300

    GR_Options-&-Managers
    Se movio los script que estaban en la carpeta art/ui a script/ui.
    Se cambio para que el antiguo mixermanager ahora es mixerUI y hay un nuevo mixermanager que si es manager y singleton.
    Se creo un prefab de el mixermanager para que los use en algunas escenas.
    Se actualizo los menus para que eso funcione.

--- Assets/_Scripts/UI/OptionMenuUI.cs
+++ Assets/_Scripts/UI/OptionMenuUI.cs
@@ -5,19 +5,19 @@ namespace Game.UI
 {
     public class OptionMenuUI : MonoBehaviour
     {
-        [SerializeField] private MixerManager _mixerManager;
+        [SerializeField] MixerUI mixerUI;
         [SerializeField] GameplayConfigManager _gameplayManager;
 
         public void ConfirmOptions()
         {
-            _mixerManager.ConfirmChange();
+            mixerUI.ConfirmChange();
             _gameplayManager.ConfirmChange();
             gameObject.SetActive(false);
         }
 
         public void CancelOptions()
         {
-            _mixerManager.CancelChange();
+            mixerUI.CancelChange();
             _gameplayManager.CancelChange();
             gameObject.SetActive(false);
         }
