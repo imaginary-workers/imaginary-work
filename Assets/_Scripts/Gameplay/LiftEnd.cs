b27dff71c4aea57f97782f0d7b89014959c251df
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2022-11-21 01:16:09 -0300

    GR_Lift
    Se cambio el mesh del ascensor por Lift Generic que se reacomodo escalas y transform, ademas se le agrego el trigger con el playerchecker para detectar cuando entre o salga el player.
    Se agrego los 3 script de Lift, LiftStart, LiftGame.
    Se agrego las dos variantes de Lift: Lift StartGame y LiftEndGame con sus componentes.

---
+++ Assets/_Scripts/Gameplay/LiftEnd.cs
@@ -0,0 +1,31 @@
+﻿using Game.Gameplay.Enemies;
+using Game.Managers;
+using Game.SO;
+using UnityEngine;
+
+namespace Game.Gameplay
+{
+    public class LiftEnd : Lift
+    {
+        [SerializeField] PlayerChecker _checker;
+            [SerializeField] SceneSO _nextScene;
+
+        void Start()
+        {
+            Enemy.OnNoEnemies += OpenDoors;
+            _checker.OnPlayerEnter += EndGame;
+        }
+
+        void OnDestroy()
+        {
+            Enemy.OnNoEnemies -= OpenDoors;
+            _checker.OnPlayerEnter -= EndGame;
+        }
+
+        public override void EndGame()
+        {
+            base.EndGame();
+            GameManager.Instance.NextScene(_nextScene);
+        }
+    }
+}
