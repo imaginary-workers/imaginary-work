25f77974943b2baaf4a7eaf2697296eb747a8f71
Zebado <sbasualdo.antares@gmail.com>
2022-12-04 19:49:53 -0300

    GR_DialogSystem
    Se agrego el PlayManager para pausar al player

---
+++ Assets/_Scripts/Managers/PlayManager.cs
@@ -0,0 +1,40 @@
+using Game.Gameplay.Player;
+using UnityEngine;
+
+namespace Game.Managers
+{
+    public class PlayManager : MonoBehaviour
+    {
+        GameObject _player;
+
+        static PlayManager _instance;
+
+        public static PlayManager Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = FindObjectOfType<PlayManager>();
+                return _instance;
+            }
+        }
+        private void Awake()
+        {
+            _instance = this;
+        }
+        private void Start()
+        {
+            _player = GameManager.Player;
+        }
+        public void SetPlayerControlActive(bool active)
+        {
+            _player.GetComponent<PlayerController>().active = active;
+            _player.GetComponent<WeaponController>().active = active;
+        }
+        public void CanvasController(bool active)
+        {
+            SetPlayerControlActive(!active);
+            Cursor.lockState = active ? CursorLockMode.Confined : CursorLockMode.Locked;
+            Time.timeScale = active ? 0 : 1;
+        }
+    }
+}
