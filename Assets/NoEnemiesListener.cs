768531999da296d0616026f18f80fa9e5dc7f2be
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2022-12-04 21:59:15 -0300

    GR
    Se agrego el noenemieslistener

---
+++ Assets/NoEnemiesListener.cs
@@ -0,0 +1,20 @@
+using Game.Dialogs;
+using Game.Gameplay.Enemies;
+using UnityEngine;
+
+namespace Game
+{
+    public class NoEnemiesListener : MonoBehaviour
+    {
+        [SerializeField] DialogEmitter _dialogEmitter;
+        private void Awake()
+        {
+            Enemy.OnNoEnemies += _dialogEmitter.Emit;
+        }
+
+        private void OnDestroy()
+        {
+            Enemy.OnNoEnemies -= _dialogEmitter.Emit;
+        }
+    }
+}
