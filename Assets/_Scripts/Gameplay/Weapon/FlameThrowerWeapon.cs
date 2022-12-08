492ddb808e258a7d16b9627b6c6d4385c4ec3fb8
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2022-12-08 16:53:08 -0300

    GR
    Se agrego un segundo object pooler para la bala especial de el firethrower

--- Assets/_Scripts/Gameplay/Weapon/FlameThrowerWeapon.cs
+++ Assets/_Scripts/Gameplay/Weapon/FlameThrowerWeapon.cs
@@ -1,17 +1,31 @@
 using Game.Gameplay.Player;
+using UnityEngine;
 
 namespace Game.Gameplay.Weapons
 {
     public class FlameThrowerWeapon : TriggerWeapon
     {
         PlayerAnimationManager _animationManager;
+        [SerializeField] ObjectPooler specialBulletPooler;
 
         public override void SubscribeToAnimationEvents(PlayerAnimationManager animationManager)
         {
             animationManager.AddAnimationEvent("start_reload", EVENT_START_RELOAD);
             animationManager.AddAnimationEvent("fire_shooting_event", EVENT_Weapon_SHOOTING);
+            animationManager.AddAnimationEvent("fire_special_shooting_event", EVENT_SPECIAL_WEAPON_SHOOTING);
             _TriggerAttackAnimation = animationManager.FireShooter;
             _animationManager = animationManager;
+        }
+
+        private void EVENT_SPECIAL_WEAPON_SHOOTING()
+        {
+            var bulletObject = specialBulletPooler.GetPooledObject();
+            bulletObject.transform.position = _firePoint.position;
+            bulletObject.SetActive(true);
+            bulletObject.transform.forward = _firePoint.forward;
+            bulletObject.GetComponent<Bullet>()?.Shoot(ShootDirection);
+            // if (_particles != null) _particles?.Play();
+            // IsShoot();
         }
 
         public override void StartSpecial()
