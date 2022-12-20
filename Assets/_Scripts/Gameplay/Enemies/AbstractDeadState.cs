f09d19cecc6eb3dc0c5625436f8a23cbaaf1b3e4
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2022-12-20 19:40:31 -0300

    GR
    Se hizo para que los enemigos solo mueran una vez

--- Assets/_Scripts/Gameplay/Enemies/AbstractDeadState.cs
+++ Assets/_Scripts/Gameplay/Enemies/AbstractDeadState.cs
@@ -4,11 +4,14 @@ namespace Game.Gameplay.Enemies
 {
     public abstract class AbstractDeadState : State
     {
+        int dead = 1;
         public GameObject Damaging { get; set; }
 
         public override void Enter()
         {
+            if (dead < 1) return;
             Enemy.SubstractEnemy();
+            dead--;
         }
     }
 }
