21dc5be0cb452c2bc2dc1cb62c6d961c827a72de
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2023-05-21 21:29:22 -0300

    GR

---
+++ Assets/_Scripts/CheatDDWeaponEnergy.cs
@@ -0,0 +1,19 @@
+﻿using Game.Gameplay.Weapons.SO;
+using Game.Managers;
+using UnityEngine;
+
+namespace Game
+{
+    public class CheatDDWeaponEnergy: MonoBehaviour
+    {
+        public WeaponSO _weaponSO;
+        
+
+        public void AddFullEnergy()
+        {
+            Debug.Log("AddFullEnergy");
+            _weaponSO.Energy = _weaponSO.MaxEnergy;
+            GameplayUIManager.Instance.UpdateEnergyBar(_weaponSO.Energy, _weaponSO.MaxEnergy);
+        }
+    }
+}
